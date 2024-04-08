5cfc40c2942624c8d48460771269199244932377
Owen-Choi <oldstyle482@gmail.com>
2024-04-08 21:30:50 +0900

    refactor : 좋아요 누른 게시글 조회 api 복구

--- src/main/java/f3f/dev1/domain/scrap/dto/ScrapPostDTO.java
+++ src/main/java/f3f/dev1/domain/scrap/dto/ScrapPostDTO.java
@@ -2,42 +2,36 @@ package f3f.dev1.domain.scrap.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 import f3f.dev1.domain.model.TradeStatus;
-import f3f.dev1.domain.scrap.dao.ScrapPostRepository;
-import lombok.*;
+import f3f.dev1.domain.trade.model.Trade;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class ScrapPostDTO {
 
 
-    @Builder
-//    @AllArgsConstructor
-    @NoArgsConstructor
     @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class GetUserScrapPost{
         private Long postId;
         private String thumbNail;
         private String title;
-
         private TradeStatus tradeStatus;
-//        private String tradeStatus;
         private String wishCategory;
         private Long scrapCount;
-        @QueryProjection
-        public GetUserScrapPost(Long postId, String title, String thumbNail, TradeStatus tradeStatus, String wishCategory, Long likeCount) {
+
+        public GetUserScrapPost(Long postId, String title, String thumbNail, Trade trade, String wishCategory, Long likeCount) {
             this.postId = postId;
             this.title = title;
             this.thumbNail = thumbNail;
-            this.tradeStatus = tradeStatus;
+            this.tradeStatus = trade != null && trade.getTradeStatus().name().equals("0") ?
+                TradeStatus.TRADABLE : trade != null && trade.getTradeStatus().name().equals("1") ?
+                TradeStatus.TRADING : TradeStatus.TRADED;
             this.wishCategory = wishCategory;
             this.scrapCount = likeCount;
         }
-
-//        public GetUserScrapPost(ScrapPostRepository.GetUserScrapPostInterface getUserScrapPostInterface) {
-//            this.postId = getUserScrapPostInterface.getPostId();
-//            this.thumbNail = getUserScrapPostInterface.getThumbnail();
-//            this.title = getUserScrapPostInterface.getTitle();
-//            this.tradeStatus = getUserScrapPostInterface.getTradeStatus();
-//            this.wishCategory = getUserScrapPostInterface.getName();
-//            this.scrapCount = getUserScrapPostInterface.getLikes();
-//        }
     }
 }
